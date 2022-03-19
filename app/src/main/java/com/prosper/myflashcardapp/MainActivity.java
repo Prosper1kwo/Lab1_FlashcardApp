78413451d0ec8b4a74d82b0c2433ca8a38fd8b93
Prosper1kwo <89759268+Prosper1kwo@users.noreply.github.com>
2022-03-19 01:59:42 -0500

    Respond to changes

--- app/src/main/java/com/prosper/myflashcardapp/MainActivity.java
+++ app/src/main/java/com/prosper/myflashcardapp/MainActivity.java
@@ -1,10 +1,17 @@
 package com.prosper.myflashcardapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView flashcardQuestion;
+    TextView flashcardAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -26,5 +33,30 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
+
+        ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
+        addQuestionImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        flashcardAnswer = findViewById(R.id.answer);
+        flashcardQuestion = findViewById(R.id.flashcard_question);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100){
+            // get data
+            if(data != null){
+                String questionStr = data.getExtras().getString("question_key");
+                String answerStr = data.getExtras().getString("answer_key");
+                flashcardQuestion.setText(questionStr);
+                flashcardAnswer.setText(answerStr);
+            }
+        }
     }
 }
