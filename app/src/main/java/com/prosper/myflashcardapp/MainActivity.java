ecf5205a69ead72bce6bd6b5a579c7ae3662a365
Prosper1kwo <89759268+Prosper1kwo@users.noreply.github.com>
2022-04-09 02:11:19 -0500

    Respond to Extensions

--- app/src/main/java/com/prosper/myflashcardapp/MainActivity.java
+++ app/src/main/java/com/prosper/myflashcardapp/MainActivity.java
@@ -2,6 +2,11 @@ package com.prosper.myflashcardapp;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.EditText;
 import android.content.Intent;
 import android.os.Bundle;
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     ImageView nextQuestion;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         flashcardAnswer = findViewById(R.id.answer);
@@ -34,8 +40,18 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                View answerSideView = findViewById(R.id.answer);
+                int cx = answerSideView.getWidth() / 2;
+                int cy = answerSideView.getHeight() / 2;
+                float finalRadius = (float) Math.hypot(cx, cy);
+                Animator anim = ViewAnimationUtils.createCircularReveal(answerSideView, cx, cy, 0f, finalRadius);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
-                findViewById(R.id.answer).setVisibility(View.VISIBLE);
+                answerSideView.setVisibility(View.VISIBLE);
+
+                anim.setDuration(3000);
+                anim.start();
+//                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.answer).setVisibility(View.VISIBLE);
             }
         });
 
@@ -50,43 +66,68 @@ public class MainActivity extends AppCompatActivity {
         ImageView addQuestionImageView = findViewById(R.id.flashcard_add_button);
         addQuestionImageView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
                 startActivityForResult(intent, 100);
+                overridePendingTransition(R.anim.right_left, R.anim.left_right);
             }
         });
 
         flashcardDatabase = new FlashcardDatabase(getApplicationContext());
 
         allFlashCards = flashcardDatabase.getAllCards();
-//
         if (allFlashCards != null && allFlashCards.size() > 0) {
-            Flashcard firstCard = allFlashCards.get(0);
-            flashcardQuestion.setText(firstCard.getQuestion());
-            flashcardAnswer.setText(firstCard.getAnswer());
+          //  Flashcard firstCard = allFlashCards.get(0);
+            flashcardQuestion.setText(allFlashCards.get(0).getQuestion());
+            flashcardAnswer.setText(allFlashCards.get(0).getAnswer());
         }
 
 
-        nextQuestion.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.flashcard_next_button).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                if (allFlashCards.size() == 0 || allFlashCards == null) {
+            public void onClick(View view) {
+                final Animation leftOutAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.left_right);
+                final Animation rightInAnim = AnimationUtils.loadAnimation(view.getContext(), R.anim.right_left);
+
+                if (allFlashCards.size() == 0) {
                     return;
                 }
 
                 cardIndex++;
 
                 if(cardIndex >= allFlashCards.size()) {
-                    Snackbar.make(v,
+                    Snackbar.make(view,
                             "You've reached the end of the cards, going back to start.",
                             Snackbar.LENGTH_SHORT)
                             .show();
                     cardIndex = 0;
 
-                    Flashcard flashcard = allFlashCards.get(cardIndex);
-                    flashcardQuestion.setText(flashcard.getAnswer());
-                    flashcardAnswer.setText(flashcard.getQuestion());
                 }
+
+                leftOutAnim.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        // this method is called when the animation first starts
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        // this method is called when the animation is finished playing
+                        findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
+                        Flashcard flashcard = allFlashCards.get(cardIndex);
+                        flashcardQuestion.setText(flashcard.getQuestion());
+                        flashcardAnswer.setText(flashcard.getAnswer());
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                        // we don't need to worry about this method
+                    }
+                });
+
+                findViewById(R.id.flashcard_question).startAnimation(rightInAnim);
+
             }
         });
 
